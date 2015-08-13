07c4f9f89a3a5b6337d0b0e9edd218d5aed304eb
jv-juven <juven-jv@foxmail.com>
2015-08-13 19:03:54 +0800

    完善借口文档，建立页面文件夹

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>
+		@yield("title","礼乎")
+	</title>
+	<meta http-equiv="keywords" content="礼乎">
+	<meta http-equiv="description" content="礼乎">
+
+	@section("css")
+	@show
+
+</head>
+<body>
+
+
+
+@section("js")
+@show
+</body>
+</html>
