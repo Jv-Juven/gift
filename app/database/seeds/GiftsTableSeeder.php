6b218de4396e16876fe492da1e06bf6953aeb001
unknown <hulinalex@163.com>
2015-08-20 11:26:04 +0800

    填充数据

--- app/database/seeds/GiftsTableSeeder.php
+++ app/database/seeds/GiftsTableSeeder.php
@@ -51,6 +51,138 @@ class GiftsTableSeeder extends Seeder {
 			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
 		]);
 
+		Gift::create([
+			'topic_id'=>2,
+			'title' => 'It\'s mine',
+			'price' =>'999',
+			'content' => '床上总是很多零碎的杂物，不知道放在哪里。有了这个宿舍床头收纳架，就不怕床上没空间了。手机、水杯、充电器统统装得下，实用性强而且色彩明亮。舒服温馨的宿舍生活就靠它了。',
+			'gift_photo_intro' =>'http://7xl6gj.com1.z0.glb.clouddn.com/Bitmap33@3x.png',
+			'scan_num' => '7859',
+			'focus_num' => '7557',
+			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
+		]);
+
+		Gift::create([
+			'topic_id'=>2,
+			'title' => 'It\'s mine',
+			'price' =>'999',
+			'content' => '床上总是很多零碎的杂物，不知道放在哪里。有了这个宿舍床头收纳架，就不怕床上没空间了。手机、水杯、充电器统统装得下，实用性强而且色彩明亮。舒服温馨的宿舍生活就靠它了。',
+			'gift_photo_intro' =>'http://7xl6gj.com1.z0.glb.clouddn.com/Bitmap33@3x.png',
+			'scan_num' => '7859',
+			'focus_num' => '7557',
+			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
+		]);
+
+		Gift::create([
+			'topic_id'=>2,
+			'title' => 'It\'s mine',
+			'price' =>'999',
+			'content' => '床上总是很多零碎的杂物，不知道放在哪里。有了这个宿舍床头收纳架，就不怕床上没空间了。手机、水杯、充电器统统装得下，实用性强而且色彩明亮。舒服温馨的宿舍生活就靠它了。',
+			'gift_photo_intro' =>'http://7xl6gj.com1.z0.glb.clouddn.com/Bitmap33@3x.png',
+			'scan_num' => '7859',
+			'focus_num' => '7557',
+			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
+		]);
+
+		Gift::create([
+			'topic_id'=>2,
+			'title' => 'It\'s mine',
+			'price' =>'999',
+			'content' => 'live younger,you need it, trust me',
+			'gift_photo_intro' =>'http://7xl6gj.com1.z0.glb.clouddn.com/Bitmap33@3x.png',
+			'scan_num' => '7859',
+			'focus_num' => '7557',
+			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
+		]);
+
+		Gift::create([
+			'topic_id'=>3,
+			'title' => 'It\'s mine',
+			'price' =>'999',
+			'content' => '床上总是很多零碎的杂物，不知道放在哪里。有了这个宿舍床头收纳架，就不怕床上没空间了。手机、水杯、充电器统统装得下，实用性强而且色彩明亮。舒服温馨的宿舍生活就靠它了。',
+			'gift_photo_intro' =>'http://7xl6gj.com1.z0.glb.clouddn.com/Bitmap33@3x.png',
+			'scan_num' => '7859',
+			'focus_num' => '7557',
+			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
+		]);
+
+		Gift::create([
+			'topic_id'=>3,
+			'title' => 'It\'s mine',
+			'price' =>'999',
+			'content' => '床上总是很多零碎的杂物，不知道放在哪里。有了这个宿舍床头收纳架，就不怕床上没空间了。手机、水杯、充电器统统装得下，实用性强而且色彩明亮。舒服温馨的宿舍生活就靠它了。',
+			'gift_photo_intro' =>'http://7xl6gj.com1.z0.glb.clouddn.com/Bitmap33@3x.png',
+			'scan_num' => '7859',
+			'focus_num' => '7557',
+			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
+		]);
+
+		Gift::create([
+			'topic_id'=>3,
+			'title' => 'It\'s mine',
+			'price' =>'999',
+			'content' => '床上总是很多零碎的杂物，不知道放在哪里。有了这个宿舍床头收纳架，就不怕床上没空间了。手机、水杯、充电器统统装得下，实用性强而且色彩明亮。舒服温馨的宿舍生活就靠它了。',
+			'gift_photo_intro' =>'http://7xl6gj.com1.z0.glb.clouddn.com/Bitmap33@3x.png',
+			'scan_num' => '7859',
+			'focus_num' => '7557',
+			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
+		]);
+
+		Gift::create([
+			'topic_id'=>3,
+			'title' => 'It\'s mine',
+			'price' =>'999',
+			'content' => 'live younger,you need it, trust me',
+			'gift_photo_intro' =>'http://7xl6gj.com1.z0.glb.clouddn.com/Bitmap33@3x.png',
+			'scan_num' => '7859',
+			'focus_num' => '7557',
+			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
+		]);
+
+		Gift::create([
+			'topic_id'=>4,
+			'title' => 'It\'s mine',
+			'price' =>'999',
+			'content' => '床上总是很多零碎的杂物，不知道放在哪里。有了这个宿舍床头收纳架，就不怕床上没空间了。手机、水杯、充电器统统装得下，实用性强而且色彩明亮。舒服温馨的宿舍生活就靠它了。',
+			'gift_photo_intro' =>'http://7xl6gj.com1.z0.glb.clouddn.com/Bitmap33@3x.png',
+			'scan_num' => '7859',
+			'focus_num' => '7557',
+			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
+		]);
+
+		Gift::create([
+			'topic_id'=>4,
+			'title' => 'It\'s mine',
+			'price' =>'999',
+			'content' => '床上总是很多零碎的杂物，不知道放在哪里。有了这个宿舍床头收纳架，就不怕床上没空间了。手机、水杯、充电器统统装得下，实用性强而且色彩明亮。舒服温馨的宿舍生活就靠它了。',
+			'gift_photo_intro' =>'http://7xl6gj.com1.z0.glb.clouddn.com/Bitmap33@3x.png',
+			'scan_num' => '7859',
+			'focus_num' => '7557',
+			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
+		]);
+
+		Gift::create([
+			'topic_id'=>4,
+			'title' => 'It\'s mine',
+			'price' =>'999',
+			'content' => '床上总是很多零碎的杂物，不知道放在哪里。有了这个宿舍床头收纳架，就不怕床上没空间了。手机、水杯、充电器统统装得下，实用性强而且色彩明亮。舒服温馨的宿舍生活就靠它了。',
+			'gift_photo_intro' =>'http://7xl6gj.com1.z0.glb.clouddn.com/Bitmap33@3x.png',
+			'scan_num' => '7859',
+			'focus_num' => '7557',
+			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
+		]);
+
+		Gift::create([
+			'topic_id'=>4,
+			'title' => 'It\'s mine',
+			'price' =>'999',
+			'content' => 'live younger,you need it, trust me',
+			'gift_photo_intro' =>'http://7xl6gj.com1.z0.glb.clouddn.com/Bitmap33@3x.png',
+			'scan_num' => '7859',
+			'focus_num' => '7557',
+			'taobao_url' =>'https://detail.tmall.com/item.htm?spm=a217e.7765654.1998660470.7.a4lzKx&id=521089667043'
+		]);
+
 	}
 
 }
