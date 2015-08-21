b64409ba8e7de34f2f7a81fe7dd644f0dfc349ae
unknown <hulinalex@163.com>
2015-08-21 13:22:20 +0800

    微信认证接口

---
+++ app/controllers/WeixinController.php
@@ -0,0 +1,36 @@
+<?php
+
+class WeixinController extends BaseController{
+	protected $token = 'ziruikeji';
+
+	protected function check_signature()
+	{
+		$signature = Input::get('signature');
+		$timestamp = Input::get('timestamp');
+		$nonce	 = Input::get('nonce');
+
+		$tmpArr = array( self::$token, $timestamp, $nonce);
+		sort( $tmpArr, SORT_STRING);
+		$tmpStr = implode($tmpArr);
+
+		return $tmpStr == $signature;	
+	}
+
+	public function repsonse_token()
+	{
+		$repsonse_text = 'Error';
+
+		if(self::check_signature())
+		{
+			$repsonse_text = Input::get('echostr');
+		};
+
+		return Response::make($repsonse_text);
+	}
+
+	public function response_message()
+	{
+		return Response::make('success');
+	}
+	
+}
--- app/routes.php
+++ app/routes.php
@@ -29,6 +29,7 @@ Route::group(array('prefix' => 'home'),function(){
 });
 
 Route::controller('/login', 'AdminController');
+Route::get('weixin', 'WeixinController@response_message');
 // Route::group(array('prefix' => 'user'),function()
 // {
 // 	//注册—获取注册页面
