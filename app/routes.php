872033f88c05ba6d62d5d86846f98dcc6c362e73
unknown <hulinalex@163.com>
2015-08-21 14:48:57 +0800

    微信认证

--- app/routes.php
+++ app/routes.php
@@ -30,6 +30,8 @@ Route::group(array('prefix' => 'home'),function(){
 
 Route::controller('/login', 'AdminController');
 Route::get('weixin', 'WeixinController@valid');
+Route::get('weixin/test', 'WeixinController@responseMsg');
+
 // Route::group(array('prefix' => 'user'),function()
 // {
 // 	//注册—获取注册页面
