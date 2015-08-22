1eff21621bb3d6cff2c8d34c0a1a884eca96837f
unknown <hulinalex@163.com>
2015-08-22 15:49:34 +0800

    微信授权测试

--- app/controllers/WeixinAuthController.php
+++ app/controllers/WeixinAuthController.php
@@ -5,7 +5,7 @@ class WeixinAuthController extends BaseController{
 	//获取code
 	public function code()
 	{
-		$redirect_uri = "http://gift.zerioi.com/weixin/access_token = ";
+		$redirect_uri = "http://gift.zerioi.com/weixin/access_token";
 		$scope = "snsapi_userinfo";
 		$redirect_url = WeChatClient::getOAuthConnectUri($redirect_uri,'', $scope);
 		return Redirect::to($redirect_url);
