64a51634a7ec84f59dac785a6ac3f43c3732a694
unknown <hulinalex@163.com>
2015-08-22 17:21:03 +0800

    微信认证测试

--- app/controllers/WeixinAuthController.php
+++ app/controllers/WeixinAuthController.php
@@ -13,15 +13,26 @@ class WeixinAuthController extends BaseController{
 
 	public function accessToken()
 	{
-		$code = Input::get('code');
-
-		$weixin_data = WeChatClient::getAccessTokenByCode($code);
-
-		$access_token = $weixin_data['access_token'];
-		$refresh_token = $weixin_data['refresh_token'];
-		$open_id 	= $weixin_data['openid'];
-		$scope_userinfo = $weixin_data['scope_userinfo'];
-
+		
+		if(Session::get('code'))
+		{
+			$access_token = $session_access_token;
+			$refresh_token = $session_refresh_token;
+			$open_id = $session_openid;
+			$session_openid = $session_scope_userinfo;
+		}else{
+			$code = Input::get('code');
+			$code = Session::put('code', $code);
+			$weixin_data = WeChatClient::getAccessTokenByCode($code);
+			$access_token = $weixin_data['access_token'];
+			$session_access_token = Session::put('session_access_token', $access_token);
+			$refresh_token = $weixin_data['refresh_token'];
+			$session_refresh_token = Session::put('session_refresh_token', $refresh_token);
+			$open_id 	= $weixin_data['openid'];
+			$session_openid = Session::put('session_openid', $open_id);
+			$scope_userinfo = $weixin_data['scope_userinfo'];
+			$session_scope_userinfo = Session::put('session_scope_userinfo',$scope_userinfo);
+		}
 		$user = WeChatClient::getUserInfoByAuth($access_token, $open_id);
 
 		return $user;
