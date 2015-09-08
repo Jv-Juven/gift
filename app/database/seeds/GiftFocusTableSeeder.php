caf3d5be3af43a0f1ac27f925215cee70ab5ba27
flyingalex <hulinalex@163.com>
2015-09-08 09:54:45 +0800

    修改数据

--- app/database/seeds/GiftFocusTableSeeder.php
+++ app/database/seeds/GiftFocusTableSeeder.php
@@ -39,5 +39,61 @@ class GiftFocusTableSeeder extends Seeder {
 			'gift_id'=>2,
 			'user_id' =>4
 		]);
+		GiftFocus::create([
+			'gift_id'=>3,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>4,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>16,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>5,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>6,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>7,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>8,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>9,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>10,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>11,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>12,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>13,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>14,
+			'user_id' =>1
+		]);
+		GiftFocus::create([
+			'gift_id'=>15,
+			'user_id' =>1
+		]);
 	}
 }
