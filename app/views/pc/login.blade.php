86fda4b3e90996fff285b18ee28a67002552e4c2
jv-juven <juven-jv@foxmail.com>
2015-09-09 16:16:18 +0800

    登录也没做完

--- app/views/pc/login.blade.php
+++ app/views/pc/login.blade.php
@@ -12,14 +12,91 @@
 @section("body")
 	<div class="page-content">
 		<img class="bg-img" src="/images/pc/login/login_bg.png">
-		<div class="login-components-wrapper login-components-bg"></div>
+		<!-- <div class="login-components-wrapper login-components-bg"></div> -->
 		<div class="login-components-wrapper login-components-board">
 			<ul class="login-board-menu">
 				<li class="active"><a href="javascript:">注册</a></li>
 				<li><a href="javascript:">登录</a></li>
 				<li><a href="javascript:">下载APP</a></li>
 			</ul>
-			<div class="login-board"></div>
+			<div id="register" class="login-board" style="display: none;">
+				<div class="login-btn QQ">
+					<a class="a-block" href="/">
+						<span class="login-btn-icon">
+							<img src="/images/pc/login/QQ.png">
+						</span>
+						<span class="login-btn-text">
+							<span>
+								使用腾讯QQ注册
+							</span>
+						</span>
+					</a>
+				</div>
+				<div class="login-btn WeChat">
+					<a class="a-block" href="/">
+						<span class="login-btn-icon">
+							<img src="/images/pc/login/wechat.png">
+						</span>
+						<span class="login-btn-text">
+							<span>
+								使用微信注册
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+			<div id="login" class="login-board">
+				<div class="login-btn QQ">
+					<a class="a-block" href="/">
+						<span class="login-btn-icon">
+							<img src="/images/pc/login/QQ.png">
+						</span>
+						<span class="login-btn-text">
+							<span>
+								使用腾讯QQ登录
+							</span>
+						</span>
+					</a>
+				</div>
+				<div class="login-btn WeChat">
+					<a class="a-block" href="/">
+						<span class="login-btn-icon">
+							<img src="/images/pc/login/wechat.png">
+						</span>
+						<span class="login-btn-text">
+							<span>
+								使用微信登录
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+			<div id="download" class="login-board">
+				<div class="login-btn QQ">
+					<a class="a-block" href="/">
+						<span class="login-btn-icon">
+							<img src="/images/pc/login/QQ.png">
+						</span>
+						<span class="login-btn-text">
+							<span>
+								使用腾讯QQ登录
+							</span>
+						</span>
+					</a>
+				</div>
+				<div class="login-btn WeChat">
+					<a class="a-block" href="/">
+						<span class="login-btn-icon">
+							<img src="/images/pc/login/wechat.png">
+						</span>
+						<span class="login-btn-text">
+							<span>
+								使用微信登录
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
 		</div>
 	</div>
 @stop
