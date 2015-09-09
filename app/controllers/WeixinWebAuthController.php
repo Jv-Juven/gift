f297a57273de000de57b9dd32d858efd8aff0a14
flyingalex <hulinalex@163.com>
2015-09-09 11:33:20 +0800

    网页微信授权测试

--- app/controllers/WeixinWebAuthController.php
+++ app/controllers/WeixinWebAuthController.php
@@ -1,11 +1,11 @@
 <?php
 
-class WeiXinWebAuthController extends BaseController{
+class WeixinWebAuthController extends BaseController{
 
-	private $appid = 'wx28b883bf7df1800e';
-	private $appsecret = 'f496493d33d58b0084079ad3b980421b';
-	
-	private static function get($url)
+    private $appid = 'wx28b883bf7df1800e';
+    private $appsecret = 'f496493d33d58b0084079ad3b980421b';
+    
+    private static function get($url)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -30,8 +30,8 @@ class WeiXinWebAuthController extends BaseController{
     }
 
 
-	//1、用户同意授权，获取code。
-	public function getOAuthConnectUri($redirect_uri, $state = '', $scope = 'snsapi_login')
+    //1、用户同意授权，获取code。
+    public function getOAuthConnectUri($redirect_uri, $state = '', $scope = 'snsapi_login')
     {
         $redirect_uri = urlencode($redirect_uri);
         $url          = "https://open.weixin.qq.com/connect/qrconnect?appid={$this->appid}&redirect_uri={$redirect_uri}&response_type=code&scope={$scope}&state={$state}#wechat_redirect";
@@ -63,43 +63,48 @@ class WeiXinWebAuthController extends BaseController{
         return $res;
     }
 
-	
-	//获取code
-	public function code()
-	{
-		$redirect_uri = "http://gift.zerioi.com/weixin/web_access";
-		$scope = "snsapi_login";
-		$redirect_url = $this->getOAuthConnectUri($redirect_uri,'', $scope);
-		return Redirect::to($redirect_url);
-	}
+    
+    //获取code
+    public function code()
+    {
+        $redirect_uri = "http://gift.zerioi.com/weixin/web_access";
+        $scope = "snsapi_login";
+        $redirect_url = $this->getOAuthConnectUri($redirect_uri,'', $scope);
+        return Redirect::to($redirect_url);
+    }
 
-	public function accessToken()
-	{
-		// if(Session::get('code') == Input::get('code'))
-		// {
-		// 	Session::put('code', Input::get('code'));
-		$code = Input::get('code');
-		$weixin_data = $this->getAccessTokenByCode($code);
-		// var_dump($weixin_data);
-		$access_token = $weixin_data['access_token'];
-		$refresh_token = $weixin_data['refresh_token'];
-		$open_id 	= $weixin_data['openid'];
+    public function accessToken()
+    {
+        Session::put('code', Input::get('code'));
+        $code = Input::get('code');
+        $weixin_data = $this->getAccessTokenByCode($code);
+        // var_dump($weixin_data);
+        $access_token = $weixin_data['access_token'];
+        $refresh_token = $weixin_data['refresh_token'];
+        $open_id    = $weixin_data['openid'];
         $unionid    = $weixin_data['unionid'];
-		$user = $this->getUserInfoByAuth($access_token, $open_id);
-		
-        $client_user = New User;
-		$client_user->username = $user['nickname'];
-		$client_user->avatar = $user['headimgurl'];
-        $client_user->gender = $user['sex'];
-        $client_user->openid = $user['openid'];
-        $client_user->unionid = $user['unionid'];
-        $client_user->save();	
-		return json_encode($client_user);
-		// }
-// 		Session::put('code',Input::get('code'));
-// //      $client_user = New User;
-// //		$client_user->username = $user['nickname'];
-// //	    $client_user->save();
-// 		return Redirect::to('/');
-	}
+        $user = $this->getUserInfoByAuth($access_token, $open_id);
+        
+        $user = User::where('unionid', '=', $unionid)->first();
+        if(!isset($user))
+        {
+            $client_user = New User;
+            $client_user->username = $user['nickname'];
+            $client_user->avatar = $user['headimgurl'];
+            $client_user->gender = $user['sex'];
+            $client_user->email = $user['unionid'];
+            $client_user->openid = $user['openid'];
+            $client_user->unionid = $user['unionid'];
+            // dd($client_user);
+            $client_user->save();
+            $user = Sentry::findUserById($client_user->id);
+            Sentry::login($user,false);
+            return Redirect::to('/')->with(array('user'->$user));
+        }
+
+        $user = Sentry::findUserById($user->id);
+        Sentry::login($user,false);
+        
+        return Redirect::to('/')->with(array('user'->$user));
+    }
 }
