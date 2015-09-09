2b15d14cf86ee59ccfee25021ffcc6e8bcf72215
flyingalex <hulinalex@163.com>
2015-09-09 16:54:38 +0800

    路由修改

--- app/routes.php
+++ app/routes.php
@@ -13,7 +13,7 @@
 //礼品展示
 Route::get('/', 'HomePageController@showWelcome');
 // Route::get("/", function (){
-// 	return Response::view("pc.login");
+// 	return Response::view("index.userCenter");
 // });	
 
 //首页模块
