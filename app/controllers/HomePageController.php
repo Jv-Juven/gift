f1d185684549f27b0fdadd3b517272be72d8391c
flyingalex <hulinalex@163.com>
2015-09-11 16:13:49 +0800

    添加话题详情是否喜欢的状态

--- app/controllers/HomePageController.php
+++ app/controllers/HomePageController.php
@@ -2,6 +2,22 @@
 
 class HomePageController extends BaseController {
 
+	public isLike()
+	{
+		if(Sentry::check())
+		{
+			$gift_focus 	= DB::table('gift_focus')->where('user_id','=', Sentry::getUser()->id)
+							->where('gift_id', '=', $gift_id)->first();
+		}
+		//判断是否收藏
+		if(isset($gift_focus))
+		{
+			$type = 1;
+		}else{
+			$type = 0;
+		}
+		return $type;
+	}
 	//首页呈现
 	public function showWelcome()
 	{
@@ -100,19 +116,8 @@ class HomePageController extends BaseController {
 		}
 
 		$gift 		= Gift::find($gift_id);
-
-		if(Sentry::check())
-		{
-			$gift_focus 	= DB::table('gift_focus')->where('user_id','=', Sentry::getUser()->id)
-							->where('gift_id', '=', $gift_id)->first();
-		}
-		//判断是否收藏
-		if(isset($gift_focus))
-		{
-			$type = 1;
-		}else{
-			$type = 0;
-		}
+		//是否喜欢
+		$type = $this->isLike($gift_id);
 
 		if( Request::wantsJson() )
 		{
