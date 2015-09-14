4ee9cef22d4693377c817f4e4cc397d5561dd7db
flyingalex <hulinalex@163.com>
2015-09-14 15:04:53 +0800

    修改七牛参数

--- app/controllers/UploadController.php
+++ app/controllers/UploadController.php
@@ -6,9 +6,9 @@ class UploadController extends BaseController {
 	public function getUpToken()
 	{
 		$accessKey = 'KnOa3BkwBgjUSOm6TqvtYoARkhGM7blkOIq7G9Mg';
-		$secretKey = '-0XmYlKCYaushdgA-R9_d87hyvYPsHDKTnVQTW2Q5';
+		$secretKey = '0XmYlKCYaushdgA-R9_d87hyvYPsHDKTnVQTW2Q5';
 		$auth = new Auth($accessKey, $secretKey);
-		$bucket = 'gift-ios';
+		$bucket = 'gift';
 		$upToken = $auth->uploadToken($bucket);
 		return Response::json(array("uptoken" => $upToken));
 	}
