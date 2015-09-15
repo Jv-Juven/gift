8b77932053c4843b838c4d503ed04875796703d9
flyingalex <hulinalex@163.com>
2015-09-15 19:42:57 +0800

    增加pc端home页面滚动图片数据

---
+++ app/config/administrator/scroll_imgs.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+	'title' => 'PC端首页滚动图片',
+
+	'single' => 'PC端首页滚动图片',
+
+	'model' => 'ScrollImg',
+
+	'columns' => array(
+		'id' =>array(
+			'title' => 'id'			
+		),
+		'img' =>array(
+			'title' => '图片'			
+		),
+	),
+
+	'edit_fields' => array(
+		'img' =>array(
+			'title' => '图片链接'			
+		),
+	),
+);	
