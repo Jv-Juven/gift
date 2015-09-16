00e2d5f096c15544088994d912c98edb83a7b0ba
hyy <376246826@qq.com>
2015-09-16 16:19:57 +0800

    修改bug

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -9,6 +9,7 @@ class HomeController extends BaseController {
 			return Response::json(array('errCode'=>10, 'message' => '请登录'));
 
 		$gift_id 	= Input::get('gift_id');
+		/* 2015-09-16 hyy 改 start */
 		$gift_focus = GiftFocus::where('user_id','=', Sentry::getUser()->id)
 							   ->where('gift_id', '=', $gift_id)->first();
 
@@ -16,7 +17,7 @@ class HomeController extends BaseController {
 		{
 			if(!$gift_focus->delete())
 				return Response::json(array('errCode'=>2, 'message'=>'取消收藏失败！'));
-			
+		/* 2015-09-16 hyy 改 end */
 			return Response::json(array('errCode'=>0, 'message'=>'cancel'));
 		}else{
 			$gift_focus = New GiftFocus;
@@ -32,9 +33,9 @@ class HomeController extends BaseController {
 	{
 		if(!Sentry::check())
 			return Response::json(array('errCode'=>10, 'message' => '请登录'));
-		
+
 		$topic_id 	= Input::get('topic_id');
-		
+		/* 2015-09-16 hyy 改 start */
 		$topic_focus = TopicFocus::where( 'user_id', Sentry::getUser()->id )
 								 ->where( 'topic_id', $topic_id )
 								 ->first();
@@ -43,6 +44,7 @@ class HomeController extends BaseController {
 		{
 			if(!$topic_focus->delete())
 				return Response::json(array('errCode'=>2, 'message'=>'取消收藏失败！'));
+		/* 2015-09-16 hyy 改 end */
 			return Response::json(array('errCode'=>0, 'message'=>'取消收藏成功！'));
 		}else{
 			$topic_focus = New TopicFocus;
