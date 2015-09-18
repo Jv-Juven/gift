28de270dba0bf171ee4e6f510ef2d83c2b9af1c6
flyingalex <hulinalex@163.com>
2015-09-18 09:19:16 +0800

    添加注册人数统计

--- app/config/packages/frozennode/administrator/administrator.php
+++ app/config/packages/frozennode/administrator/administrator.php
@@ -68,6 +68,7 @@ return array(
 				'prices'
 			),
 			'scroll_imgs',
+			'statistic'
 		),
 
 	/**
---
+++ app/models/Statistic.php
@@ -0,0 +1,11 @@
+<?php
+
+class Statistic extends Eloquent{
+
+	protected $table = 'statistic';
+
+	protected $fillable = array(
+				'qq',
+				'weixin'
+	);
+}		
